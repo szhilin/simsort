759003f7d44d6d7ff1a267a62e98ca2697c006e9
Sergei Zhilin <szhilin@gmail.com>
2015-10-02 17:02:05 +0600

    Add init simulation parameters as StartFcn callback

---
+++ initsimsort.m
@@ -0,0 +1,11 @@
+function initsimsort()
+%INITSIMSORT Setups SimSort simulation parameterse
+
+Params=setparam('raisin_results_CSandSimple.mat'); 
+R1=Params.B1(1);
+R2=Params.B2(1);
+
+disp('SimSort parameters initialized.')
+
+end
+
